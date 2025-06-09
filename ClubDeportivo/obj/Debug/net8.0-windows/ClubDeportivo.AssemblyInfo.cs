251ed3813d45f2977427f47a4e3f038d274eb4c9
Adrian <madronala@gmail.com>
2025-06-09 00:48:07 -0300

    Cambios en logo - diseño

--- ClubDeportivo/obj/Debug/net8.0-windows/ClubDeportivo.AssemblyInfo.cs
+++ ClubDeportivo/obj/Debug/net8.0-windows/ClubDeportivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8bdf65c273d785ef9e439a06aebc666d19036ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d800635a49eb47c3b1099ab1ef63cecd192a5aab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
