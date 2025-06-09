a1405840ecc2ba111085c28a5c477a13364f8288
Adrian <madronala@gmail.com>
2025-06-09 17:53:05 -0300

    subiendo la base

--- ClubDeportivo/obj/Debug/net8.0-windows/ClubDeportivo.AssemblyInfo.cs
+++ ClubDeportivo/obj/Debug/net8.0-windows/ClubDeportivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ec22b05e478ee4c7af84999a892bf188873454")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356b24f07705b693b31fbd5f8625d7e4f25eb570")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
